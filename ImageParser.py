37b57df9e573da27a99275a251ff50a621dcb7cc
S.Miroshin <s.miroshin@butik.ru>
2019-04-10 18:32:28 +0300

    rewrite ImageParser

--- ImageParser.py
+++ ImageParser.py
@@ -1,33 +1,56 @@
+import csv
 import json
 import urllib.request
 from pathlib import Path
 from tqdm import tqdm
 
 
-def __get(url: str) -> json:
+def __make_dir(path: Path):
+    if not path.is_dir():
+        path.mkdir()
+
+def __save_images(url: str, path: Path):
+    for images in __get_response(url):
+        for count in tqdm(range(len(images.get("images")))):
+            last_segment = __get_last_segment(images.get("images")[count])
+            if path.joinpath(last_segment).is_file():
+                continue
+            urllib.request.urlretrieve(images.get("images")[count],
+                                       path.joinpath(last_segment))
+
+
+def __get_response(url: str) -> json:
     return json.load(urllib.request.urlopen(url=url)).get("data")
 
 
-def get_last_segment(url: str) -> str:
+def __get_last_segment(url: str) -> str:
     return str(Path(url).name.split("?")[0])
 
 
-def image_parser():
-    with open("url") as f:
-        for lines in f.readlines():
-            if not lines.strip():
-                continue
-            line = lines.split(" ")
-            path = Path(line[1].rstrip())
-            if not path.is_dir():
-                path.mkdir()
-            for images in __get(line[0]):
-                for count in tqdm(range(len(images.get("images")))):
-                    last_segment = get_last_segment(images.get("images")[count])
-                    if path.joinpath(last_segment).is_file():
-                        continue
-                    urllib.request.urlretrieve(images.get("images")[count],
-                                               path.joinpath(last_segment))
+def __csv_reader(file_name: str):
+    file = Path(file_name)
+    if not file.is_file():
+        raise Exception("file not found")
+
+    with open(file, "r") as f:
+        reader = csv.reader(f, delimiter=';')
+        for line in reader:
+            path = Path(line[1])
+            __make_dir(path)
+            __save_images(line[0],path)
+
+
+def image_parser(file_name: str = "url.csv"):
+    file = Path(file_name)
+    if not file.is_file():
+        raise Exception("file not found")
+
+    with open(file, "r") as f:
+        reader = csv.reader(f, delimiter=';')
+        for line in reader:
+            path = Path(line[1])
+            __make_dir(path)
+            __save_images(line[0],path)
 
 
 if __name__ == "__main__":
