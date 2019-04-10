e59e3b4d28eef8520381b251dc7472f128f5a122
S.Miroshin <s.miroshin@butik.ru>
2019-04-10 14:59:02 +0300

    Remove requests, rewrite open file

--- ImageParser.py
+++ ImageParser.py
@@ -1,25 +1,27 @@
-import requests
+import json
 import urllib.request
+import os
 from tqdm import tqdm
 
-def __get(url:str):
-    return requests.get(url=url).json().get("data")
+
+def __get(url:str) -> json:
+    return json.load(urllib.request.urlopen(url=url))
+
 
 def image_parser():
     counter = 0
-    import os
-    f = open("url")
-    for lines in f.readlines():
-        line = lines.split(" ")
-        if not os.path.isdir(line[1]):
-            os.makedirs(line[1])
-        for images in __get(line[0]):
-            for count in tqdm(range(len(images.get("images")))):
-                if os.path.isfile(line[1] +"\\" + str(counter) + ".jpg"):
+    with open("url") as f:
+        for lines in f.readlines():
+            line = lines.split(" ")
+            if not os.path.isdir(line[1]):
+                os.makedirs(line[1])
+            for images in __get(line[0]):
+                for count in tqdm(range(len(images.get("images")))):
+                    if os.path.isfile(line[1] +"\\" + str(counter) + ".jpg"):
+                        counter += 1
+                        continue
+                    urllib.request.urlretrieve(images.get("images")[count],
+                                                line[1] +"\\" + str(counter) + ".jpg")
                     counter += 1
-                    continue
-                urllib.request.urlretrieve(images.get("images")[count],
-                                            line[1] +"\\" + str(counter) + ".jpg")
-                counter += 1
 
 image_parser()
