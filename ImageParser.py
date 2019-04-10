2b0f642c7b3dfb1a938599b91e7605d7b5a87da7
S.Miroshin <s.miroshin@butik.ru>
2019-04-10 17:34:38 +0300

    change os to pathlib

--- ImageParser.py
+++ ImageParser.py
@@ -1,6 +1,6 @@
 import json
 import urllib.request
-import os
+from pathlib import Path
 from tqdm import tqdm
 
 
@@ -8,23 +8,24 @@ def __get(url: str) -> json:
     return json.load(urllib.request.urlopen(url=url)).get("data")
 
 
-def get_last_segment(url:str) -> str:
-    return str(os.path.basename(url).split("?")[0])
+def get_last_segment(url: str) -> str:
+    return str(Path(url).name.split("?")[0])
 
 
 def image_parser():
     with open("url") as f:
         for lines in f.readlines():
             line = lines.split(" ")
-            if not os.path.isdir(line[1]):
-                os.makedirs(line[1])
+            path = Path(line[1])
+            if not path.is_dir():
+                path.mkdir()
             for images in __get(line[0]):
                 for count in tqdm(range(len(images.get("images")))):
                     last_segment = get_last_segment(images.get("images")[count])
-                    if os.path.isfile(line[1] + "\\" + last_segment + ".jpg"):
+                    if path.joinpath(last_segment).is_file():
                         continue
                     urllib.request.urlretrieve(images.get("images")[count],
-                                               line[1] + "\\" + last_segment + ".jpg")
+                                               path.joinpath(last_segment))
 
 
 if __name__ == "__main__":
