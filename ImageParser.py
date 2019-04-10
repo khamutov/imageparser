846c15ada123ac7acdef7a8d9c532cdec468c157
S.Miroshin <s.miroshin@butik.ru>
2019-04-10 18:54:33 +0300

    change reader to DictReader

--- ImageParser.py
+++ ImageParser.py
@@ -28,30 +28,17 @@ def __get_last_segment(url: str) -> str:
     return str(Path(url).name.split("?")[0])
 
 
-def __csv_reader(file_name: str):
-    file = Path(file_name)
-    if not file.is_file():
-        raise Exception("file not found")
-
-    with open(file, "r") as f:
-        reader = csv.reader(f, delimiter=';')
-        for line in reader:
-            path = Path(line[1])
-            __make_dir(path)
-            __save_images(line[0], path)
-
-
 def image_parser(file_name: str = "url.csv"):
     file = Path(file_name)
     if not file.is_file():
         raise Exception("file not found")
 
     with open(file, "r") as f:
-        reader = csv.reader(f, delimiter=';')
-        for line in reader:
-            path = Path(line[1])
+        reader = csv.DictReader(f, ["url", "dir_name"], delimiter=";")
+        for row in reader:
+            path = Path(row["dir_name"])
             __make_dir(path)
-            __save_images(line[0], path)
+            __save_images(row["url"], path)
 
 
 if __name__ == "__main__":
