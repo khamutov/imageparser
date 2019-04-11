46c5479edb58dcb07d745cc5d9b8df10e7df697d
S.Miroshin <s.miroshin@butik.ru>
2019-04-11 19:20:11 +0300

    rewrite ImageParse, add generators

--- ImageParser.py
+++ ImageParser.py
@@ -6,17 +6,18 @@ from tqdm import tqdm
 
 
 class ImageParser:
+    def __collect_images(self, resp) -> []:
+        return [image for images_list in resp for image in images_list["images"]]
+
     def __make_dir(self, path: Path):
         if not path.is_dir():
             path.mkdir()
 
     def __save_images(self, url: str, path: Path):
-        resp = self.__get_response(url)
-        for image_list in tqdm(resp, desc='load categories      '):
-            for image in tqdm(image_list.get("images"), desc='load image_list     '):
-                last_segment = self.__get_last_segment(image)
-                if path.joinpath(last_segment).is_file():
-                    continue
+        arr = self.__collect_images(self.__get_response(url))
+        for image in tqdm(arr, desc="download: "):
+            last_segment = self.__get_last_segment(image)
+            if not path.joinpath(last_segment).is_file():
                 urllib.request.urlretrieve(image,
                                            path.joinpath(last_segment))
 
