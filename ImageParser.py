1890008dea27c729529db5e9d5b3f9cd6bfdbc48
S.Miroshin <s.miroshin@butik.ru>
2019-04-11 11:29:20 +0300

    rewrite ImageParse, add class

--- ImageParser.py
+++ ImageParser.py
@@ -5,42 +5,39 @@ from pathlib import Path
 from tqdm import tqdm
 
 
-def __make_dir(path: Path):
-    if not path.is_dir():
-        path.mkdir()
+class ImageParser:
+    def __make_dir(self, path: Path):
+        if not path.is_dir():
+            path.mkdir()
 
+    def __save_images(self, url: str, path: Path):
+        resp = self.__get_response(url)
+        for image_list in tqdm(resp, desc='load categories      '):
+            for image in tqdm(image_list.get("images"), desc='load image_list     '):
+                last_segment = self.__get_last_segment(image)
+                if path.joinpath(last_segment).is_file():
+                    continue
+                urllib.request.urlretrieve(image,
+                                           path.joinpath(last_segment))
 
-def __save_images(url: str, path: Path):
-    resp = __get_response(url)
-    for images_count in tqdm(range(len(resp)), desc='load categories      '):
-        for count_image in tqdm(range(len(resp[images_count].get("images"))), desc='load images     '):
-            last_segment = __get_last_segment(resp[images_count].get("images")[count_image])
-            if path.joinpath(last_segment).is_file():
-                continue
-            urllib.request.urlretrieve(resp[images_count].get("images")[count_image],
-                                       path.joinpath(last_segment))
+    def __get_response(self, url: str) -> str:
+        return json.load(urllib.request.urlopen(url=url)).get("data")
 
+    def __get_last_segment(self, url: str) -> str:
+        return str(Path(url).name.split("?")[0])
 
-def __get_response(url: str) -> json:
-    return json.load(urllib.request.urlopen(url=url)).get("data")
+    def image_parser(self, file_name: str = "url.csv"):
+        file = Path(file_name)
+        if not file.is_file():
+            raise Exception("file not found")
 
-
-def __get_last_segment(url: str) -> str:
-    return str(Path(url).name.split("?")[0])
-
-
-def image_parser(file_name: str = "url.csv"):
-    file = Path(file_name)
-    if not file.is_file():
-        raise Exception("file not found")
-
-    with open(file, "r") as f:
-        reader = csv.DictReader(f, ["url", "dir_name"], delimiter=";")
-        for row in reader:
-            path = Path(row["dir_name"])
-            __make_dir(path)
-            __save_images(row["url"], path)
+        with open(file, "r") as f:
+            reader = csv.DictReader(f, ["url", "dir_name"], delimiter=";")
+            for row in reader:
+                path = Path(row["dir_name"])
+                self.__make_dir(path)
+                self.__save_images(row["url"], path)
 
 
 if __name__ == "__main__":
-    image_parser()
+    ImageParser().image_parser()
