4e55792fa4c09dcadb34d5b80dfcbfe8332fbbfe
Miroshin Sergey <sm@whenspeak.ru>
2019-04-20 20:12:19 +0300

    add default dirs

--- MoveImagesToFolder.py
+++ MoveImagesToFolder.py
@@ -7,10 +7,12 @@ from tqdm import tqdm
 valid_pct=0.2
 train_pct=0.2
 
+dir_name= ["train","test","validation"]
+
 class MoveImages:
-    __array_images = []
-    def __init__(self,array:[]):
-        self.__array_images = array
+    def __init__(self,array_images:[],array_dirs : [] = dir_name):
+        self.__array_images = array_images
+        self.__array_dirs = array_dirs
 
     def __move_images_to_folder(self,path_src: Path, path_dist: Path, dir_name: str, images: []):
         for image in tqdm(images, desc="move images to {} len {}".format(dir_name, len(images))):
@@ -20,6 +22,7 @@ class MoveImages:
     def move_images(self, path: Path):
         validation_idx = round(len(self.__array_images, ) * valid_pct)
         test_idx = round(validation_idx + len(self.__array_images) * train_pct)
-        self.__move_images_to_folder(path, path.joinpath("train"), "train", self.__array_images[:validation_idx])
-        self.__move_images_to_folder(path, path.joinpath("test"), "test", self.__array_images[validation_idx: test_idx])
-        self.__move_images_to_folder(path, path.joinpath("validation"), "validation", self.__array_images[test_idx:])
+        for dir in tqdm(self.__array_dirs,desc="Move files"):
+            self.__move_images_to_folder(path, path.joinpath(dir), dir, self.__array_images[:validation_idx])
+            self.__move_images_to_folder(path, path.joinpath(dir), dir, self.__array_images[validation_idx: test_idx])
+            self.__move_images_to_folder(path, path.joinpath(dir), dir, self.__array_images[test_idx:])
