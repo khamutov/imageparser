cc052aa8d505a2db6388312d3312fe299a56df34
Miroshin Sergey <sm@whenspeak.ru>
2019-04-20 09:36:23 +0300

    add asyncio

--- ImageParser.py
+++ ImageParser.py
@@ -1,15 +1,22 @@
 import csv
 import json
+import random
 import shutil
 import urllib.request
 import os
+import asyncio
 from pathlib import Path
+
+import aiohttp as aiohttp
 from tqdm import tqdm
 
 from Decorators import make_dirs
 
+sem = asyncio.Semaphore(3)
+
 
 def move_images_to_folder(path: Path, arr: [], file="categories.csv"):
+    random.shuffle(arr)
     with open(file, "r") as f:
         reader = csv.DictReader(f, ["section", "percent"], delimiter=";")
         last = int(0)
@@ -20,6 +27,7 @@ def move_images_to_folder(path: Path, arr: [], file="categories.csv"):
                                 dst=path.joinpath(row.get("section")).joinpath(image))
             last = round(last + round(len(arr) * int(row.get("percent"))) / 100)
 
+
 class ImageParser:
     def __get_images_array(self, url: str) -> []:
         array = []
@@ -28,12 +36,22 @@ class ImageParser:
                 array.append(image)
         return array
 
-    def __save_images(self, url: str, dir_name: str, path: Path):
-        for image in tqdm(self.__get_images_array(url), desc=dir_name + ": Download images"):
-            last_segment = self.__get_last_segment(image)
-            if not path.joinpath(last_segment).is_file():
-                urllib.request.urlretrieve(image,
-                                           str(path.joinpath(last_segment)))
+    async def __fetch(self, url, session, path: Path):
+        async with sem:
+            async with session.get(url) as response:
+                with open(path.joinpath(self.__get_last_segment(str(response.url))), 'wb') as f:
+                    f.write(await response.read())
+
+
+    async def __save_images(self, url: str, path: Path):
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for image in (self.__get_images_array(url)):
+                if not path.joinpath(self.__get_last_segment(image)).is_file():
+                    tasks.append(self.__fetch(image, session, path))
+
+            for task in tqdm(tasks, desc="download: "):
+                await task
 
         move_images_to_folder(path, [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])
 
@@ -41,8 +59,10 @@ class ImageParser:
     def __get_response(self, url: str) -> str:
         return json.load(urllib.request.urlopen(url=url)).get("data")
 
+
     def __get_last_segment(self, url: str) -> str:
         return str(Path(url).name.split("?")[0])
+
 
     def image_parser(self, file_name: str = "url.csv"):
         file = Path(file_name)
@@ -54,7 +74,8 @@ class ImageParser:
             for row in tqdm(reader, desc="Done: "):
                 path = Path(row["dir_name"])
                 make_dirs(path)
-                self.__save_images(row["url"], row["dir_name"], path)
+                loop = asyncio.get_event_loop()
+                loop.run_until_complete(self.__save_images(row["url"], path))
 
 
 if __name__ == "__main__":
