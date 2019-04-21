6de181df2f6f973bae51da318bd5b1caba5f0b5c
Miroshin Sergey <sm@whenspeak.ru>
2019-04-21 18:36:43 +0300

    change brand to product_variation, add images

--- CollectMeta.py
+++ CollectMeta.py
@@ -12,9 +12,11 @@ class Meta:
     def collect_meta(self, dir_name: str, data : [] ):
         with open("meta.csv","w",newline='') as file:
             for item in data.get("data"):
-                csv_reader = csv.writer(file,delimiter=';')
-                csv_reader.writerow([dir_name,
-                                     [f.split("?")[0]for f in item["images"]],
-                                     item["brand_id"],
-                                     item["brand_name"]])
+                for image in item["images"]:
+                    for product_variations in item["product_variations"]:
+                        csv_reader = csv.writer(file,delimiter=';')
+                        csv_reader.writerow([dir_name,
+                                             image.split("?")[0],
+                                             product_variations["product_variation_id"],
+                                             item["brand_name"]])
 
