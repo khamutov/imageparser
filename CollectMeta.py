abcf6debd16ceea368cd40d73dc86efbeef60751
Miroshin Sergey <sm@whenspeak.ru>
2019-04-21 19:25:40 +0300

    change product_variation to photobank_id

--- CollectMeta.py
+++ CollectMeta.py
@@ -1,22 +1,20 @@
 # coding=utf-8
 import csv
 
+
 class Meta:
 
     def __write_to_file(self, data: []):
-        with open("meta.csv","w") as file:
-            csv_reader = csv.writer(file,delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        with open("meta.csv", "w") as file:
+            csv_reader = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             csv_reader.writerow(data)
 
-
-    def collect_meta(self, dir_name: str, data : [] ):
-        with open("meta.csv","w",newline='') as file:
+    def collect_meta(self, dir_name: str, data: []):
+        with open("meta.csv", "w", newline='') as file:
             for item in data.get("data"):
                 for image in item["images"]:
-                    for product_variations in item["product_variations"]:
-                        csv_reader = csv.writer(file,delimiter=';')
-                        csv_reader.writerow([dir_name,
-                                             image.split("?")[0],
-                                             product_variations["product_variation_id"],
-                                             item["brand_name"]])
-
+                    csv_reader = csv.writer(file, delimiter=';')
+                    csv_reader.writerow([dir_name,
+                                         image.split("?")[0],
+                                         item["photobank_id"],
+                                         item["brand_name"]])
