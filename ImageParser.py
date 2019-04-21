b2be9557b1ff45beef2f406f4e263fb57fd941ae
Miroshin Sergey <sm@whenspeak.ru>
2019-04-21 11:59:56 +0300

    add page count for image parser

--- ImageParser.py
+++ ImageParser.py
@@ -2,6 +2,7 @@
 import csv
 import json
 import urllib.request
+import urllib.parse
 import asyncio
 from pathlib import Path
 
@@ -13,34 +14,37 @@ from MoveImagesToFolder import MoveImages
 sem = asyncio.Semaphore(3)
 
 file_name = "url.csv"
+max_page = 30
 
 class ImageParser:
-    def __get_images_array(self, url: str) -> []:
+    def __get_images_array(self, url: str, page_params: {} = {"page": 1}) -> []:
         array = []
-        for item in self.__get_response(url):
+        for item in self.__get_response(url, urllib.parse.urlencode(page_params)):
             for image in item.get("images"):
                 array.append(image)
         return array
 
     async def __fetch(self, path_to: Path, url, session, path: Path):
         async with sem:
-            async with session.get(url) as response:
+            async with session.get(url=url) as response:
                 temp = path_to.joinpath(path.joinpath(self.__get_last_segment(str(response.url))))
                 with open(temp, 'wb') as f:
                     f.write(await response.read())
 
     async def __save_images(self, path_to: Path, url: str, dir_name: str, path: Path):
-        async with aiohttp.ClientSession() as session:
-            tasks = []
-            for image in (self.__get_images_array(url)):
-                if not path_to.joinpath(path.joinpath(self.__get_last_segment(image))).is_file():
-                    tasks.append(self.__fetch(path_to, image, session, path))
+        tempCount = 1
+        while tempCount != max_page and len(self.__get_images_array(url, {"page" : tempCount})) != 0:
+            async with aiohttp.ClientSession() as session:
+                tasks = []
+                for image in (self.__get_images_array(url, {"page" : tempCount})):
+                    if not path_to.joinpath(path.joinpath(self.__get_last_segment(image))).is_file():
+                        tasks.append(self.__fetch(path_to, image, session, path))
+                for task in tqdm(tasks, desc="download {}".format(dir_name)):
+                    await task
+                tempCount += 1
 
-            for task in tqdm(tasks, desc="download {}".format(dir_name)):
-                await task
-
-    def __get_response(self, url: str) -> str:
-        return json.load(urllib.request.urlopen(url=url)).get("data")
+    def __get_response(self, url: str, params) -> str:
+        return json.load(urllib.request.urlopen(url +"?" + params)).get("data")
 
     def __get_last_segment(self, url: str) -> str:
         return str(Path(url).name.split("?")[0])
