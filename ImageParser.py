688a31319dcce22b9dd48c1a592614df6ea3170c
S.Miroshin <s.miroshin@butik.ru>
2019-04-22 17:08:49 +0300

    remove unused params

--- ImageParser.py
+++ ImageParser.py
@@ -17,12 +17,13 @@ sem = asyncio.Semaphore(3)
 file_name = "url.csv"
 max_page = 1
 
+
 class ImageParser:
     def __get_images_array(self, url: str, page_params: {} = {"page": 1}) -> []:
         array = []
         for item in self.__get_response(url, urllib.parse.urlencode(page_params)):
             for image in item.get("images"):
-                array.append(image)
+                array.append(image.split("?")[0])
         return array
 
     async def __fetch(self, path_to: Path, url, session, path: Path):
@@ -34,10 +35,10 @@ class ImageParser:
 
     async def __save_images(self, path_to: Path, url: str, dir_name: str, path: Path):
         tempCount = 1
-        while tempCount <= max_page and len(self.__get_images_array(url, {"page" : tempCount})) != 0:
+        while tempCount <= max_page and len(self.__get_images_array(url, {"page": tempCount})) != 0:
             async with aiohttp.ClientSession() as session:
                 tasks = []
-                for image in (self.__get_images_array(url, {"page" : tempCount})):
+                for image in (self.__get_images_array(url, {"page": tempCount})):
                     if not path_to.joinpath(path.joinpath(self.__get_last_segment(image))).is_file():
                         tasks.append(self.__fetch(path_to, image, session, path))
                 for task in tqdm(tasks, desc="download {} page {}".format(dir_name, tempCount)):
@@ -45,23 +46,21 @@ class ImageParser:
                 tempCount += 1
 
     def __get_response(self, url: str, params) -> str:
-        resp = json.load(urllib.request.urlopen(url +"?" + params))
-        Meta().collect_meta(dir_name=self.dir_name,data=resp)
+        resp = json.load(urllib.request.urlopen(url + "?" + params))
+        Meta().collect_meta(dir_name=self.dir_name, data=resp)
         return resp.get("data")
 
     def __get_last_segment(self, url: str) -> str:
         return str(Path(url).name.split("?")[0])
 
-    def image_parser(self, path_to: Path, file_name: str = "url.csv"):
-        file = Path(file_name)
-        with open(file, "r") as f:
-            reader = csv.DictReader(f, ["url", "dir_name"], delimiter=";")
-            for row in tqdm(reader):
-                self.dir_name = row["dir_name"]
-                path = Path(self.dir_name)
-                path_to.joinpath(path).mkdir(parents=True, exist_ok=True)
-                loop = asyncio.get_event_loop()
-                loop.run_until_complete(self.__save_images(path_to,row["url"], self.dir_name, path))
+    def image_parser(self, path_to: Path):
+        reader = csv.DictReader(f, ["url", "dir_name"], delimiter=";")
+        for row in tqdm(reader):
+            self.dir_name = row["dir_name"]
+            path = Path(self.dir_name)
+            path_to.joinpath(path).mkdir(parents=True, exist_ok=True)
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(self.__save_images(path_to, row["url"], self.dir_name, path))
 
 
 if __name__ == "__main__":
