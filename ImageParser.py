ba64c406f2b3dcafec46143a17dbbda293c44cac
S.Miroshin <s.miroshin@butik.ru>
2019-04-22 19:01:29 +0300

    refactoring: rewrite __get_images_array, async __save_images, __get_response

--- ImageParser.py
+++ ImageParser.py
@@ -16,15 +16,16 @@ from MoveImagesToFolder import MoveImages
 sem = asyncio.Semaphore(3)
 
 file_name = "url.csv"
-max_page = 1
+max_page = 2
 
 
 class ImageParser:
-    def __get_images_array(self, url: str, page_params: {} = {"page": 1}) -> []:
+    def __get_images_array(self, data: [], path_to: Path) -> []:
         array = []
-        for item in self.__get_response(url, urllib.parse.urlencode(page_params)):
+        for item in data:
             for image in item.get("images"):
-                array.append(image.split("?")[0])
+                if not path_to.joinpath(self.__get_last_segment(image)).is_file():
+                    array.append(image.split("?")[0])
         return array
 
     async def __fetch(self, path_to: Path, dir_name: Path, url: str):
@@ -35,18 +36,21 @@ class ImageParser:
                     async with aiof.open(temp, 'wb') as f:
                         await f.write(await response.read())
 
-    async def __save_images(self, path_to: Path, url: str, dir_name: str, path: Path):
-        tempCount = 1
-        while tempCount <= max_page and len(self.__get_images_array(url, {"page": tempCount})) != 0:
-            images = self.__get_images_array(url, {"page": tempCount})
-            for image in tqdm(images):
-                await self.__fetch(path_to=path_to,dir_name=path, url=image)
-            tempCount += 1
+    async def __save_images(self, path_to: Path, path_dir_name: Path, url: str):
+        self.last_page = -1
+        for i in range(1, max_page + 1):
+            if self.last_page == max_page:
+                break
+            response = self.__get_response(url=url, params={"page": i})
+            self.last_page = response["last_page"]
+            image_array = self.__get_images_array(data=response.get("data"), path_to=path_to.joinpath(path_dir_name))
+            for image in tqdm(image_array, desc="download label {}".format(path_dir_name)):
+                await self.__fetch(path_to=path_to, dir_name=path_dir_name, url=image)
 
-    def __get_response(self, url: str, params) -> str:
-        resp = json.load(urllib.request.urlopen(url + "?" + params))
+    def __get_response(self, url: str, params={"page": 1}) -> str:
+        resp = json.load(urllib.request.urlopen(url + "?" + urllib.parse.urlencode(params)))
         Meta().collect_meta(dir_name=self.dir_name, data=resp)
-        return resp.get("data")
+        return resp
 
     def __get_last_segment(self, url: str) -> str:
         return str(Path(url).name.split("?")[0])
@@ -58,7 +62,7 @@ class ImageParser:
             path = Path(self.dir_name)
             path_to.joinpath(path).mkdir(parents=True, exist_ok=True)
             loop = asyncio.get_event_loop()
-            loop.run_until_complete(self.__save_images(path_to, row["url"], self.dir_name, path))
+            loop.run_until_complete(self.__save_images(path_to, path, row["url"]))
 
 
 if __name__ == "__main__":
