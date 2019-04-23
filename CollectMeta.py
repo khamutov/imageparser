caa580e2f8ac7be043223d5ea1158cc23f8979eb
S.Miroshin <s.miroshin@butik.ru>
2019-04-23 17:16:39 +0300

    add @classmethod

--- CollectMeta.py
+++ CollectMeta.py
@@ -3,13 +3,14 @@ import csv
 
 
 class Meta:
-
-    def __write_to_file(self, data: []):
+    @classmethod
+    def __write_to_file(cls, data: []):
         with open("meta.csv", "w") as file:
             csv_reader = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             csv_reader.writerow(data)
 
-    def collect_meta(self, dir_name: str, data: []):
+    @classmethod
+    def collect_meta(cls, dir_name: str, data: []):
         with open("meta.csv", "w", newline='') as file:
             for item in data.get("data"):
                 for image in item["images"]:
